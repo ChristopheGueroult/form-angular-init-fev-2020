0cf02cc81f1bfc1c828203b350209a3991ba8911
christophe gueroult <christophegueroult@gmail.com>
2021-02-15 17:06:20 +0100

    LoginModule et LoginRoutingModule and components pages

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { UiModule } from '../ui/ui.module';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginModule } from '../login/login.module';
 
 
 
@@ -14,6 +15,7 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   exports: [
     UiModule,
+    LoginModule,
     HeaderComponent,
     NavComponent,
     FooterComponent
